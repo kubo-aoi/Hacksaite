e78d825ce4529ae92f0072f6e9be21b0413a9280
kubo-aoi <c0a2104428@edu.teu.ac.jp>
2023-07-20 16:00:12 +0900

    addcss

--- address_process.php
+++ address_process.php
@@ -1,4 +1,55 @@
 <!-- address_process.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>ログイン結果</title>
+    <a href="top_page.cgi"><h1>
+    <h1><img src= "./button/ComBuy.png" width="320"height="100"></h1>
+    <style>
+        body {
+            background:linear-gradient(180deg, #000000 0%, #000000 27%, #fffaf0 27%, #fffaf0 100%);
+            background-repeat:no-repeat;
+            font-family: Arial, sans-serif;
+            background-color:#fffaf0;
+        }
+
+        .container {
+            max-width: 400px;
+            margin: 0 auto;
+            padding: 40px;
+            background-color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+        }
+
+        h1 {
+            text-align: center;
+            color: #333;
+            margin-top: 0;
+        }
+
+        .error-message {
+            color: #ff0000;
+            text-align: center;
+            margin-bottom: 20px;
+        }
+
+        .success-message {
+            color: #00cc00;
+            text-align: center;
+            margin-bottom: 20px;
+        }
+
+        .back-link {
+            display: block;
+            text-align: center;
+            margin-top: 20px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
 <?php
 // データベース接続情報
 $host = "localhost";
@@ -34,12 +85,12 @@ try {
 
         if ($stmt_update->execute()) {
             echo '<h1>更新完了</h1>';
-            echo '<div>住所情報が更新されました。</div>';
-            echo '<a href="top_page.cgi">戻る</a>';
+            echo '<div class="success-message">住所情報が更新されました。</div>';
+            echo '<a class="back-link" href="top_page.cgi">戻る</a>';
         } else {
             echo '<h1>エラー</h1>';
-            echo '<div>更新に失敗しました。</div>';
-            echo '<a href="touroku.php">戻る</a>';
+            echo '<div class="error-message">更新に失敗しました。</div>';
+            echo '<a class="back-link" href="touroku.php">戻る</a>';
         }
     } else {
         // 住所情報が登録されていない場合は新規登録
@@ -52,19 +103,22 @@ try {
 
         if ($stmt_insert->execute()) {
             echo '<h1>登録完了</h1>';
-            echo '<div>住所情報が登録されました。</div>';
-            echo '<a href="top_page.cgi">戻る</a>';
+            echo '<div class="success-message">住所情報が登録されました。</div>';
+            echo '<a class="back-link" href="top_page.cgi">戻る</a>';
         } else {
             echo '<h1>エラー</h1>';
-            echo '<div>登録に失敗しました。</div>';
-            echo '<a href="touroku.php">戻る</a>';
+            echo '<div class="error-message">登録に失敗しました。</div>';
+            echo '<a class="back-link" href="touroku.php">戻る</a>';
         }
     }
 
 } catch (PDOException $e) {
     echo '<h1>エラー</h1>';
-    echo '<div>データベース接続エラー: ' . $e->getMessage() . '</div>';
-    echo '<a href="touroku.php">戻る</a>';
+    echo '<div class="error-message">データベース接続エラー: ' . $e->getMessage() . '</div>';
+    echo '<a class="back-link" href="touroku.php">戻る</a>';
 }
 ?>
+</div>
+</body>
+</html>
 
